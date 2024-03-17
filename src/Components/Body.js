e4fc364274d725977a0a047aa8acf938d37f8cea
febin123 <febsep96@gmail.com>
2024-03-17 21:44:48 +0530

    fetch swiggys live api using useEffect hook

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { resObj } from "../utils/constants"
 import RestaurantCard from './RestaurantCard';
 
 const Body=()=>{
 
     const[listOfRestaurant,setListOfRestaurant]=useState(resObj)
+
+    useEffect(()=>{
+        fetchData()
+    },[])
    
+    const fetchData=async()=>{
+        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        
+        const json=await data.json()
+        console.log(json)
+        console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+
+        setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    }
+    console.log("Body rendered")
     return(
         <div className='body'>
             <div className="filter">
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ const Header=()=>{
     return(
         <div className='header'>
             <div className='logo-container'> 
-                <img className='logo' src="https://img.freepik.com/premium-vector/food-pin-app-icon-restaurant-location-logo-illustration_709422-33.jpg?w=740" alt="" srcset="" />
+                <img className='logo' src="https://img.freepik.com/premium-vector/food-pin-app-icon-restaurant-location-logo-illustration_709422-33.jpg?w=740" alt=""/>
             </div>
             <div className="nav-items">
                 <ul>

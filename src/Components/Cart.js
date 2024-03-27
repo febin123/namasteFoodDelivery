83c1173dd8dffb2ac1accacc4742d618c07f98c7
febin123 <febsep96@gmail.com>
2024-03-27 07:01:14 +0530

    installed testing library

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -15,6 +15,7 @@ const Cart=()=>{
         <h1 className="text-2xl font-bold">Cart</h1>
         <div className="m-auto w-6/12">
             <button className="p-2 m-2 bg-black text-white rounded-lg" onClick={handleClearCart}>Clear Cart</button>
+            {cartItems.length === 0 && <h1>Cart is empty please Add items to the Cart</h1>}
             <ItemList items={cartItems}/>
 
         </div>
